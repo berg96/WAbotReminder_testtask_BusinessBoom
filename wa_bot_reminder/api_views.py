86eb7b1455aea12a96346360f2c7da12d612e5ea
Artem Kulikov <chigar2010@yandex.ru>
2025-02-06 18:18:06 +0200

    one more fix, don't send fckn msg

--- wa_bot_reminder/api_views.py
+++ wa_bot_reminder/api_views.py
@@ -3,11 +3,23 @@ from datetime import datetime, timedelta
 import pytz
 from flask import request, Response
 from sqlalchemy import select
+from twilio.twiml.messaging_response import MessagingResponse
 
-from . import app, db
+from settings import Config
+from . import app, db, client
 from .crud import reminder_crud
 from .models import Reminder, User
 from .tasks import send_whatsapp_message
+
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,  # или DEBUG для более подробного логирования
+    format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+)
+
+logger = logging.getLogger(__name__)
 
 
 @app.route('/webhook', methods=['POST'])
@@ -15,6 +27,7 @@ def webhook():
     from_number = request.form.get('From')
     user = get_or_create_user(from_number, db.session)
     body = request.form.get('Body', '').strip()
+    response = MessagingResponse()
     if not from_number or not body:
         return Response('Некорректные данные', status=400)
     parts = body.split(' ', 1)
@@ -22,26 +35,41 @@ def webhook():
     args = parts[1] if len(parts) > 1 else ''
     try:
         if command == '/set_timezone':
-            user.tz = args.strip()
+            user.tz = int(args.strip())
             db.session.commit()
             db.session.refresh(user)
-            return Response(
-                f'Установлен часовой пояс UTC{"+" + str(user.tz) if user.tz >= 0 else user.tz}',
-                mimetype='text/plain'
-            )
+            response.message(f'Установлен часовой пояс UTC{"+" + str(user.tz) if user.tz >= 0 else user.tz}')
+            return str(response), 200, {'Content-Type': 'application/xml'}
         elif command == 'список':
+            logger.info('зашел в список')
             reminders = reminder_crud.get_multi(user_number=user.phone_number, session=db.session)
+            logger.info('собрал данные')
+            logger.info(f'{reminders is not None}')
+            logger.info(f'{reminders}')
+
             if reminders:
                 response_message = 'Ваши напоминания:\n'
+                logger.info(f'response_message: {response_message}')
+
                 for reminder in reminders:
+                    logger.info(f'id: {reminder.id}')
+                    logger.info(f'время: {reminder.remind_time.strftime("%Y-%m-%d %H:%M:%S")}')
+                    logger.info(f'текст: {reminder.text}')
+
                     response_message += (
                         f'ID: {reminder.id} | '
-                        f'{reminder.reminder_time.strftime("%Y-%m-%d %H:%M:%S")} '
-                        f'| {reminder.reminder_text}\n'
+                        f'{(reminder.remind_time + timedelta(hours=user.tz)).strftime("%Y-%m-%d %H:%M:%S")} '
+                        f'| {reminder.text}\n'
                     )
+                    logger.info(f'response_message: {response_message}')
+
             else:
                 response_message = 'У вас нет активных напоминаний.'
-            return Response(response_message, mimetype='text/plain')
+            logger.info(f'Перед отправкой')
+            logger.info(f'response_message: {response_message[:-1]}')
+            # return Response(response_message, mimetype='text/plain')
+            response.message(response_message)
+            return str(response), 200, {'Content-Type': 'application/xml'}
         elif command == 'удали':
             reminder = reminder_crud.get(
                 obj_id=args.strip(), user_id=user.id, session=db.session
@@ -62,32 +90,62 @@ def webhook():
             from dateutil import parser
             local_dt = parser.parse(time_str.strip())
             remind_time = local_dt - timedelta(hours=user.tz)
-            now = datetime.utcnow().replace(tzinfo=pytz.utc)
+            now = datetime.utcnow()
             if remind_time <= now:
                 return Response('Время должно быть в будущем!', mimetype='text/plain')
             reminder = Reminder(user_id=user.id, remind_time=remind_time, text=text)
+            logger.info('создал запись1')
             db.session.add(reminder)
             db.session.commit()
+            db.session.refresh(reminder)
+            logger.info('обновил запись')
             send_whatsapp_message.apply_async(args=[from_number, text], eta=remind_time)
-            return Response(
+            # return Response(
+            #     f'Напоминание запланировано на '
+            #     f'{(remind_time + timedelta(hours=user.tz)).strftime("%Y-%m-%d %H:%M")}',
+            #     mimetype='text/plain'
+            # )
+
+            response.message(
                 f'Напоминание запланировано на '
-                f'{(remind_time + timedelta(hours=user.tz)).strftime("%Y-%m-%d %H:%M")}',
-                mimetype='text/plain'
+                f'{(remind_time + timedelta(hours=user.tz)).strftime("%Y-%m-%d %H:%M")}'
             )
+            return str(response), 200, {'Content-Type': 'application/xml'}
+
         if user.tz is None:
-            return Response(
+            msg = (
                 'Установите ваш часовой пояс с помощью команды \n'
                 '"/set_timezone <разница от UTC> "\n'
                 'Пример: /set_timezone 3 (Москва)',
-                mimetype='text/plain'
             )
-        return Response(
+            # return Response(
+            #     'Установите ваш часовой пояс с помощью команды \n'
+            #     '"/set_timezone <разница от UTC> "\n'
+            #     'Пример: /set_timezone 3 (Москва)',
+            #     mimetype='text/plain'
+            # )
+            send_whatsapp_message(user.phone_number, msg)
+        # return Response(
+        #     'Для добавления напоминания: напомни <ISO время>: <текст>\n'
+        #     'Для получения списка: список\n'
+        #     'Для удаления напоминания: удали <id>\n'
+        #     'Для установки часового пояса: /set_timezone <разница от UTC>',
+        #     mimetype='text/plain'
+        # )
+        msg = (
             'Для добавления напоминания: напомни <ISO время>: <текст>\n'
             'Для получения списка: список\n'
             'Для удаления напоминания: удали <id>\n'
-            'Для установки часового пояса: /set_timezone <разница от UTC>',
-            mimetype='text/plain'
+            'Для установки часового пояса: /set_timezone <разница от UTC>'
         )
+        # response.message(
+        #     'Для добавления напоминания: напомни <ISO время>: <текст>\n'
+        #     'Для получения списка: список\n'
+        #     'Для удаления напоминания: удали <id>\n'
+        #     'Для установки часового пояса: /set_timezone <разница от UTC>'
+        # )
+        # return str(response), 200, {'Content-Type': 'application/xml'}
+        return send_whatsapp_message(user.phone_number, msg)
     except Exception as e:
         db.session.rollback()
         return Response(f'Внутренняя ошибка сервера: {e}', status=500)
@@ -103,3 +161,17 @@ def get_or_create_user(phone_number, session):
         session.commit()
         session.refresh(user)
     return user
+
+
+def send_whatsapp_message(to_number, message_body):
+    try:
+        message = client.messages.create(
+            body=message_body,
+            from_=Config.TWILIO_WHATSAPP_NUMBER,  # номер отправителя из WhatsApp Sandbox или вашего аккаунта
+            to=f'whatsapp:{to_number}'       # номер получателя в формате "whatsapp:+<номер>"
+        )
+        print(f"Message sent, SID: {message.sid}")
+        return message.sid
+    except Exception as e:
+        print(f"Ошибка при отправке сообщения: {e}")
+        return None
